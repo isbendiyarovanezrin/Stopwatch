08716d72b0a1d44f33ca3b5a9f5fa7dc3120941e
Nəzrin <isbndyrva@gmail.com>
2022-02-23 14:55:45 +0400

    Add app.js

---
+++ js/app.js
@@ -0,0 +1,49 @@
+"use strict";
+
+const btnStart = document.querySelector(".btn-start");
+const btnStop = document.querySelector(".btn-stop");
+const btnReset = document.querySelector(".btn-reset");
+const appendTens = document.getElementById("tens");
+const appendSeconds = document.getElementById("seconds");
+let tens = 0;
+let seconds = 0;
+let interval;
+
+btnStart.addEventListener("click", () => {
+  clearInterval(interval);
+  interval = setInterval(startTimer, 10);
+});
+
+btnStop.addEventListener("click", stopTimer);
+btnReset.addEventListener("click", resetTimer);
+
+function startTimer() {
+  tens++;
+
+  if (tens <= 9) {
+    appendTens.innerHTML = `0${tens}`;
+  } else if (tens > 9 && tens < 100) {
+    appendTens.innerHTML = tens;
+  } else {
+    seconds++;
+    tens = 0;
+    appendTens.innerHTML = "00";
+    appendSeconds.innerHTML = `0${seconds}`;
+  }
+
+  if (seconds > 9) {
+    appendSeconds.innerHTML = seconds;
+  }
+}
+
+function stopTimer() {
+  clearInterval(interval);
+}
+
+function resetTimer() {
+  clearInterval(interval);
+  tens = 0;
+  seconds = 0;
+  appendTens.innerHTML = "00";
+  appendSeconds.innerHTML = "00";
+}
